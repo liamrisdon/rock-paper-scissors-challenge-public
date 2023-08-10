7027d82b12ce124ce9fc9c1a43cc620a4e66f1a1
Liam Risdon <liamrisdon22@gmail.com>
2023-08-10 16:30:01 +0100

    routes tests added for full coverage

--- test/routes.test.js
+++ test/routes.test.js
@@ -3,12 +3,13 @@ import { expect } from "chai";
 import chaiHttp from "chai-http";
 import App from "../App.js";
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 
 describe("Routes tests", () => {
 
     it('/POST game test ', async () => {
+
         const res = await chai.request(App)
             .post("/game")
             .send({ player: 'Liam' });
@@ -16,5 +17,25 @@ describe("Routes tests", () => {
         expect(res).to.have.status(200);
         expect(res.body).to.be.an('object');
 
-    })
-})
+    });
+
+    it('/POST results test', async () => {
+
+        const res = await chai.request(App)
+            .post("/result")
+            .send({ choice: 'Spock' });
+
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+
+    });
+
+    it('/GET render index test', async () => {
+
+        const res = await chai.request(App)
+            .get("/");
+
+        expect(res).to.have.status(200);
+
+    });
+});
